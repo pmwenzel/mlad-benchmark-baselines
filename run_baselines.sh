751aa3e319cca459a4627023d5c38cabbf7d6166
Nan Yang <yangnan719@gmail.com>
2020-05-29 20:06:13 +0200

    use cuda for extracting superpoint by default

--- run_baselines.sh
+++ run_baselines.sh
@@ -9,7 +9,7 @@ export folder_dir=$1
 export sequence_id=$2
 
 echo "Extracting superpoint"
-python extract_superpoint.py --dataset-path $folder_dir --test-sequence $sequence_id
+python extract_superpoint.py --cuda --dataset-path $folder_dir --test-sequence $sequence_id
 
 echo "Extracting d2-net"
 python extract_d2-net.py --dataset-path $folder_dir --test-sequence $sequence_id
